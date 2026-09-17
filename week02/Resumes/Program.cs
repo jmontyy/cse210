eca49839e601319130e0d72cca5afca5dbe95a52
jmontyy <joshmontgomerymmiv@gmail.com>
2026-09-17 13:42:25 -0500

    learning activity w2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,19 @@
+using Resumes;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume r = new()
+        {
+            _name = "Todd",
+            _jobs = [
+            new() { _company = "Google", _jobTitle = "Programmer", _startYear = 2002, _endYear = 2022},
+            new() { _company = "Microsoft", _jobTitle = "Developer", _startYear = 2022, _endYear = 2026}
+            ]
+        };
+
+        r.Display();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resumes
+{
+    public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = [];
+
+        public void Display() {
+            Console.WriteLine(_name + "\nJobs:");    
+        foreach (var item in _jobs)
+            {
+                item.Display();
+            }
+        }
+    }
+}
